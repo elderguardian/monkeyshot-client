5a62e852a4bff8e317518e041301c192533691e0
elderguardian <soydev@riseup.net>
2023-05-21 21:10:40 +0200

    fixed desktop icon not starting

--- install.sh
+++ install.sh
@@ -5,4 +5,5 @@ rmdir src/
 mv files/kagerou-client ~/.local/bin/
 chmod +x ~/.local/bin/kagerou-client
 mv files/kagerou-client.desktop ~/.local/share/applications/
+mv files/kagerou-client.png ~/.local/share/icons/hicolor/512x512/apps/
 rmdir files/
