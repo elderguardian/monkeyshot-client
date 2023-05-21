8a55f0b423d7dda499e0708a1ca5df6a61085309
elderguardian <soydev@riseup.net>
2023-05-21 20:28:44 +0200

    fixed isntaller

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@ mkdir /opt/Kagerou
 mv src/* /opt/Kagerou
 rmdir src/
 
-mv files/kagerou-client /usr/local/bin/
-mv files/kagerou-client.desktop /usr/share/applications/
+mv files/kagerou-client ~/.local/bin/
+chmod +x ~/.local/bin/kagerou-client
+mv files/kagerou-client.desktop ~/.local/share/applications/
 rmdir files/
